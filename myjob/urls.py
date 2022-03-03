b19d8be384d9f745dc82037b9b8f7d174341bb21
haozemin <Haozeminpython@163.com>
2022-03-03 18:03:28 +0800

    fourth commit

--- myjob/urls.py
+++ myjob/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),不能重复定义会出现错误：?: (urls.W005) URL namespace 'admin' isn't unique. You may not be able to reverse all URLs in this namespace
     #2.新增应用的路由配置信息
     path('',views.get_index_page)
 ]
